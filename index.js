d2f65f6a327ed36e48d07e1a15f4af59a2fd83bc
SchokoTets <moritzleni@gmail.com>
2018-12-06 22:59:25 -0800

    removed semicolons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,20 +9,20 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017'
 
 // Database Name
-const dbName = 'roboticsfunding-log';
+const dbName = 'roboticsfunding-log'
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url)
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+  assert.equal(null, err)
+  console.log("Connected successfully to server")
 
-  const db = client.db(dbName);
+  const db = client.db(dbName)
 
-  client.close();
-});
+  client.close()
+})
