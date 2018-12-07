75d44f17de0203317a4554a23a7f7a08e81eac92
SchokoTets <moritzleni@gmail.com>
2018-12-06 23:02:32 -0800

    code sort

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
+
+const dburl = 'mongodb://localhost:27017'
+const dbname = 'roboticsfunding-log'
+
+const client = new MongoClient(dburl)
+
+client.connect(function(err) {
+  assert.equal(null, err)
+  console.log("Connected successfully to server")
+
+  const db = client.db(dbname)
+
+  client.close()
+})
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// Connection URL
-const url = 'mongodb://localhost:27017'
-
-// Database Name
-const dbName = 'roboticsfunding-log'
-
-// Create a new MongoClient
-const client = new MongoClient(url)
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err)
-  console.log("Connected successfully to server")
-
-  const db = client.db(dbName)
-
-  client.close()
-})
