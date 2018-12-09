e717c97f4e92770918da92894f7fca796d5fce56
schokotets <moritzleni@gmail.com>
2018-12-09 18:30:34 +0000

    added CORS heades

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const port = 8081
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if(req.method === "OPTIONS") {
+          res.header("Access-Control-Allow-Methods", "PUT, POST, GET");
+          res.status(200).json({});
+          return;
+        }
+    next();
+});
+
 const basepath = '/roboticslog/'
 app.use(basepath, express.static('.'))
 //app.use(express.json())
